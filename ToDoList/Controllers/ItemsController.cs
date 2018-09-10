6160c3d2ae7e22e5acecba3d736b8f193b58b072
Chan Lee and Kenneth Du <kenneth@>
2018-09-10 16:38:11 -0700

    add initial files for todo list

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+      List<Item> allItems = new List<Item> {new Item("test description")};
+      return View(allItems);
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/items")]
+    public ActionResult Create()
+    {
+      Item newItem = new Item(Request.Form["new-item"]);
+      List<Item> allItems = new List<Item> {newItem};
+      return View("Index", allItems);
+    }
+  }
+}
